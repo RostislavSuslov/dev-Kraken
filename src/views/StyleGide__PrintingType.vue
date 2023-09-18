a1122f7d21ee79b85d9cd65a49495af8a6abd8ba
RostSuslov <suslovros@gmail.com>
2023-09-18 16:55:47 +0300

    content variables done

--- src/views/StyleGide__PrintingType.vue
+++ src/views/StyleGide__PrintingType.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="component-item">
         <ul class="printing-list">
-             <li class="printing-item"><h1>Lorem ipsum dolor sit amet, consectetur adipisicing.</h1></li>
-             <li class="printing-item"><h2>Lorem ipsum dolor sit amet, consectetur adipisicing.</h2></li>
-             <li class="printing-item"><h3>Lorem ipsum dolor sit amet, consectetur adipisicing.</h3></li>
-             <li class="printing-item"><h4>Lorem ipsum dolor sit amet, consectetur adipisicing.</h4></li>
-             <li class="printing-item"><h5>Lorem ipsum dolor sit amet, consectetur adipisicing.</h5></li>
-             <li class="printing-item"><h6>Lorem ipsum dolor sit amet, consectetur adipisicing.</h6></li>
+            <li class="printing-item"><h1 class="text-h1">Lorem ipsum dolor sit amet, consectetur adipisicing.</h1></li>
+            <li class="printing-item"><h2 class="text-h2">Lorem ipsum dolor sit amet, consectetur adipisicing.</h2></li>
+            <li class="printing-item"><h3 class="text-h3">Lorem ipsum dolor sit amet, consectetur adipisicing.</h3></li>
+            <li class="printing-item"><h4 class="text-h4">Lorem ipsum dolor sit amet, consectetur adipisicing.</h4></li>
+            <li class="printing-item"><h5 class="text-h5">Lorem ipsum dolor sit amet, consectetur adipisicing.</h5></li>
+            <li class="printing-item"><h6 class="text-h6">Lorem ipsum dolor sit amet, consectetur adipisicing.</h6></li>
+        </ul>
+        <ul class="printing-list">
+            <li class="printing-item"><p class="text-caption">Caption</p></li>
+            <li class="printing-item"><p class="text-button">button</p></li>
+            <li class="printing-item"><p class="text-buttonSmall">Button small</p></li>
+            <li class="printing-item"><p class="text-paragraph">Paragraph</p></li>
+            <li class="printing-item"><p class="text-link">Link</p></li>
         </ul>
     </div>
 </template>
