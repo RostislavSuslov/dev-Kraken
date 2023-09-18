b02bd77192394e21fe2fbc7e5253510663f650f0
RostSuslov <suslovros@gmail.com>
2023-09-18 16:05:01 +0300

    color variables done

--- src/views/StyleGide__Color.vue
+++ src/views/StyleGide__Color.vue
@@ -10,118 +10,114 @@
 
         <ul class="color-list">
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-blue"></div>
+                <div class="color-title">Blue</div>
+                <div class="color-hex">#DBE6ED</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-brandBlue"></div>
+                <div class="color-title">Brand Blue</div>
+                <div class="color-hex">#4288B5</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
-            </li>
-        </ul>
-
-        <ul class="color-list">
-            <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
-            </li>
-            <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-blueSecondary"></div>
+                <div class="color-title">Blue Secondary</div>
+                <div class="color-hex">#072E53</div>
             </li>
         </ul>
 
         <ul class="color-list">
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-purple"></div>
+                <div class="color-title">Purple</div>
+                <div class="color-hex">#F4DCFF</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
-            </li>
-        </ul>
-
-        <ul class="color-list">
-            <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
-            </li>
-            <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-brandPurple"></div>
+                <div class="color-title">brand Purple</div>
+                <div class="color-hex">#BA90CE</div>
             </li>
         </ul>
 
         <ul class="color-list">
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-red"></div>
+                <div class="color-title">red</div>
+                <div class="color-hex">#F2E1E0</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-redSecondary"></div>
+                <div class="color-title">red Secondary</div>
+                <div class="color-hex">#9B1C13</div>
+            </li>
+        </ul>
+
+        <ul class="color-list">
+            <li class="color-item">
+                <div class="color-show bg-green"></div>
+                <div class="color-title">Green</div>
+                <div class="color-hex">#D1E7DD</div>
+            </li>
+            <li class="color-item">
+                <div class="color-show bg-greenSecondary"></div>
+                <div class="color-title">green Secondary</div>
+                <div class="color-hex">#0F5132</div>
+            </li>
+        </ul>
+
+        <ul class="color-list">
+            <li class="color-item">
+                <div class="color-show bg-gold"></div>
+                <div class="color-title">Gold</div>
+                <div class="color-hex">#FFF3CD</div>
+            </li>
+            <li class="color-item">
+                <div class="color-show bg-goldSecondary"></div>
+                <div class="color-title">Gold Secondary</div>
+                <div class="color-hex">#664D03</div>
             </li>
         </ul>
         
         <ul class="color-list">
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-light"></div>
+                <div class="color-title">Light</div>
+                <div class="color-hex">#FFFFFF</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-gray"></div>
+                <div class="color-title">Gray</div>
+                <div class="color-hex">#F6F6FA</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-graySecondary"></div>
+                <div class="color-title">gray Secondary</div>
+                <div class="color-hex">#E9E9E9</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-grayFourth"></div>
+                <div class="color-title">gray Fourth</div>
+                <div class="color-hex">#323232</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-dark"></div>
+                <div class="color-title">Dark</div>
+                <div class="color-hex">#0E3046</div>
             </li>
         </ul>
 
         <ul class="color-list">
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-darkSecondary"></div>
+                <div class="color-title">Dark Secondary</div>
+                <div class="color-hex">#202637</div>
             </li>
             <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
+                <div class="color-show bg-darkThird"></div>
+                <div class="color-title">Dark Third</div>
+                <div class="color-hex">#001223</div>
             </li>
-            <li class="color-item">
-                <div class="color-show"></div>
-                <div class="color-title">Gradient</div>
-                <div class="color-hex">#9F5AB8 <br> #1E99B4</div>
-            </li>
+            
         </ul>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        gradient: 'var(--primary)',
+        gradient:        'var(--primary)',
+        blue:            'var(--blue)',
+        brandBlue:       'var(--brandBlue)',
+        blueSecondary:   'var(--blueSecondary)',
+        purple:          'var(--purple)',
+        brandPurple:     'var(--brandPurple)',
+        red:             'var(--red)',
+        redSecondary:    'var(--redSecondary)',
+        green:           'var(--green)',
+        greenSecondary:  'var(--greenSecondary)',
+        gold:            'var(--gold)',
+        goldSecondary:   'var(--goldSecondary)',
+        light:           'var(--Light)',
+        gray:            'var(--gray)',
+        graySecondary:   'var(--graySecondary)',
+        grayThird:       'var(--grayThird)',
+        grayFourth:      'var(--grayFourth)',
+        dark:            'var(--dark)',
+        darkSecondary:   'var(--darkSecondary)',
+        darkThird:       'var(--darkThird)',
       }
     },
   },
