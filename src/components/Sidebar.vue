8312414dcd46d64b523189dbd4eb712260c866b0
RostSuslov <suslovros@gmail.com>
2023-09-19 17:45:52 +0300

    mere file

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -26,17 +26,17 @@ onMounted(() => {
 </script>
 
 <template>
-    <div class="sidebar bg-secondaryColor">
+    <div class="sidebar">
         <a href="/" class="logo">
             <img src="src/assets/logo.svg" alt="logo-Kraken-CRM">
         </a>
         <button class="burger">burger</button>
         <nav class="main-navigation">
-            <a href="offers" class="text-paragraph px-5 hover:text-accentGold">offers</a>
-            <a href="advertisers" class="text-paragraph px-5 hover:text-accentGold">advertisers</a>
-            <a href="Webmasters" class="text-paragraph px-5 hover:text-accentGold">Webmasters</a>
-            <a href="domens" class="text-paragraph px-5 hover:text-accentGold">domens</a>
-            <a href="settings" class="text-paragraph px-5 hover:text-accentGold">settings</a>
+            <a href="offers">offers</a>
+            <a href="advertisers">advertisers</a>
+            <a href="Webmasters">Webmasters</a>
+            <a href="domens">domens</a>
+            <a href="settings">settings</a>
         </nav>
         <nav class="user-navigation">
             <a href="#">User Name</a>.
