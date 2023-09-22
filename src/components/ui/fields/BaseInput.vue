a9b628edc6e13f8164de10c290c05e6b93394f5d
RostSuslov <suslovros@gmail.com>
2023-09-22 18:09:33 +0300

    baseInput

---
+++ src/components/ui/fields/BaseInput.vue
@@ -0,0 +1,20 @@
+<template>
+    <ui-fields-wrapper>
+        <input :type="type"/>
+        <template #helperText="data">
+            <slot name="helperText" v-bind="data"></slot>
+        </template>
+    </ui-fields-wrapper>
+</template>
+
+
+<script setup>
+import UiFieldsWrapper from './Wrapper.vue'
+
+defineProps({
+    type:{
+        type:String,
+        default: 'text'
+    }
+ })
+</script>
--- src/components/ui/fields/Wrapper.vue
+++ src/components/ui/fields/Wrapper.vue
@@ -1,7 +1,28 @@
 <template>
-
+    <div>
+        <slot name="label">
+            <label v-if="label">
+                {{ label }}
+            </label>
+        </slot>
+        <slot name="halperText" :helper-text="currentHelperText">
+            <span :class="{'text-red-700': isError}">
+                {{ currentHelperText }}
+            </span>
+        </slot>
+    </div>
 </template>
 
 
-<script>
+<script setup>
+import { computed } from 'vue';
+
+defineProps({
+    label: String,
+    errpr: String,
+    helperText: String,
+})
+
+const currentHelperText = computed(()=> props.error ? props.error : props.helperText);
+const isError = computed(()=> !!props.error)
 </script>
